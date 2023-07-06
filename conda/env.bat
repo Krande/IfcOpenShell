99421a9206e27ab1534fc7fef022cbc3eb349edb
krande <kristoffer_andersen@outlook.com>
2023-07-06 09:40:54 +0200

    make cityjson optional and improve support for cmake boolean flags from env strings

--- conda/env.bat
+++ conda/env.bat
@@ -11,18 +11,25 @@
 :: You have to add a .env file to the root of the project where you set PREFIX=<path to your conda env>
 set CONDA_BUILD=1
 set MY_PY_VER=311
-:: this will read the .env file and set the environment variables
-for /f delims= %%x in ('type .env') do set %%x
+
+:: set this file's parent directory as a variable
+set THIS_DIR=%~dp0
+
+:: read the .env file located in THIS_DIR and set the environment variables.
+:: the .env file should contain a line like this:
+:: PREFIX=C:\Users\your_user_name\mambaforge3\envs\ifcopenshell-build
+for /f "tokens=*" %%i in (%THIS_DIR%.env) do set %%i
 
 set LIBRARY_PREFIX=%PREFIX%/Library
 set CMAKE_PREFIX_PATH=%PREFIX%;%LIBRARY_PREFIX%/include;%LIBRARY_PREFIX%/lib;%LIBRARY_PREFIX%/bin
 
-set OCC_LIBRARY_DIR=%LIBRARY_PREFIX%/lib/cmake/opencascade
+set OCC_LIBRARY_DIR=%LIBRARY_PREFIX%/lib
 set OCC_INCLUDE_DIR=%LIBRARY_PREFIX%/include/opencascade
 
 set CGAL_DIR=%LIBRARY_PREFIX%/lib/cmake/CGAL
+set EIGEN_DIR=%LIBRARY_PREFIX%/include/eigen3
 set LIBXML2_INCLUDE_DIR=%LIBRARY_PREFIX%/include/libxml2
-set LIBXML2_LIBRARY=%LIBRARY_PREFIX%/lib/libxml2.lib
+set LIBXML2_LIBRARIES=%LIBRARY_PREFIX%/lib/libxml2.lib
 set Boost_LIBRARY_DIR=%LIBRARY_PREFIX%/lib
 set Boost_INCLUDE_DIR=%LIBRARY_PREFIX%/include
 set Boost_USE_STATIC_LIBS=OFF
