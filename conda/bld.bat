acccbded833bc6842ef3d94ed3780eec1ba48ad7
krande <kristoffer_andersen@outlook.com>
2023-07-10 14:15:20 +0200

    use newer occt and cityjson OFF

--- conda/bld.bat
+++ conda/bld.bat
@@ -37,6 +37,7 @@ cmake -G "Ninja" ^
  -D Boost_LIBRARY_DIR:FILEPATH="%LIBRARY_PREFIX%\lib" ^
  -D Boost_INCLUDE_DIR:FILEPATH="%LIBRARY_PREFIX%\include" ^
  -D Boost_USE_STATIC_LIBS:BOOL=OFF ^
+ -D CITYJSON_SUPPORT:BOOL=OFF ^
  %SRC_DIR%/cmake
 
 if errorlevel 1 exit 1
