e947c8e84b7ce7b264441e1adf8456f657bf319f
krande <kristoffer_andersen@outlook.com>
2023-08-10 10:11:46 +0200

    use swig_dir in addition to swig_exe

--- conda/build.sh
+++ conda/build.sh
@@ -13,6 +13,7 @@ if [ `uname` == Darwin ]; then
 fi
 
 cmake -G Ninja \
+ -DSWIG_EXECUTABLE=$PREFIX/bin/swig \
  -DSWIG_DIR=$PREFIX/bin \
  -DSCHEMA_VERSIONS="2x3;4;4x3;4x3_add1" \
  -DCMAKE_BUILD_TYPE=Release \
